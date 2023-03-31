4bd3b8d0397b0c8d6a0cbe263144ef314573719a
Phil Bouchet <pjbouchet@gmail.com>
2023-03-31 10:50:53 +0100

    Update zzz

--- R/zzz.R
+++ R/zzz.R
@@ -12,7 +12,7 @@
   )
   options(tibble.width = Inf)
   options(pillar.sigfig = 5)
-  Rcpp::compileAttributes()
+  # Rcpp::compileAttributes()
   # system.file(package = "data.table")
   # Rcpp::sourceCpp("src/simtools.cpp")
   # suppressWarnings(source("R/narw.R"))
