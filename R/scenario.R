4f341ee02d37ed65225a11b015522a1fcbbc118e
Phil Bouchet <pjbouchet@gmail.com>
2024-04-09 08:47:11 +0100

    Add scenario function

---
+++ R/scenario.R
@@ -0,0 +1,12 @@
+#' Offshore wind scenario
+#'
+#' Launches an interactive Shiny application to define custom scenarios for use in the bioenergetic model.
+#' 
+#' @export
+#' @author Rob Schick, Phil J. Bouchet
+#' @return An object of class \code{narwscenario}.
+
+scenario <- function(){
+
+  
+} # End function
