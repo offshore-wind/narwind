5334308d1b7ec229e41992c90034654762ce58d6
Phil Bouchet <pjbouchet@gmail.com>
2024-05-24 12:31:34 +0100

    Update plot.narwproj

--- R/plot.narwproj.R
+++ R/plot.narwproj.R
@@ -3,7 +3,9 @@
 #' Takes one or more \code{narwproj} object(s) and creates plots of the estimated population trajectories.
 #'
 #' @param ... One or more objects of class \code{narwproj}, as returned by \link{predict.narwsim}.
-#' @param diff Logical. If \code{TRUE}, returns plots of the differences between pairs of projections. Defaults to \code{FALSE}.
+#' @param diff Logical. If \code{TRUE}, returns plots of the average differences between projections. Defaults to \code{FALSE}.
+#' @param all.diff Logical. Whether the plots returned by \code{diff} should include individual differences between replicate projections.
+#' @param cohen Logical. If \code{TRUE}, assesses the magnitude of differences using Cohen's D, where small (d = 0.2), medium (d = 0.5), and large (d ≥ 0.8).
 #' @param interval Logical. If \code{TRUE}, percentile confidence intervals are shown on the plots. Defaults to \code{TRUE}.
 #' @param shade Logical. If \code{TRUE}, confidence intervals are displayed as colored ribbons. Otherwise, confidence bounds are shown as dotted lines. Defaults to \code{TRUE}.
 #' @param cohort Logical. If \code{TRUE}, separate plots are returned for each cohort. If \code{FALSE}, a single plot for the whole population is shown. Defaults to \code{FALSE}.
@@ -30,6 +32,8 @@
 #' }
 plot.narwproj <- function(...,
                           diff = FALSE,
+                          all.diff = FALSE,
+                          cohen = FALSE,
                           interval = TRUE,
                           shade = TRUE,
                           cohort = FALSE,
@@ -38,6 +42,7 @@ plot.narwproj <- function(...,
                           timeline = FALSE,
                           timeline.y = 0,
                           scales = "free",
+                          alpha = 0.3,
                           nudge.y = 20,
                           ncol = 2,
                           nx = 5,
@@ -57,84 +62,144 @@ plot.narwproj <- function(...,
   
   if("vignette" %in% names(args)) vignette <- args[["vignette"]] else vignette <- FALSE
   
-    # Function to remove some of the axis labels in ggplot faceted plots
-    gtable_filter_remove <- function (x, name, trim = TRUE){
-      matches <- !(x$layout$name %in% name)
-      x$layout <- x$layout[matches, , drop = FALSE]
-      x$grobs <- x$grobs[matches]
-      if (trim) 
-        x <- gtable::gtable_trim(x)
-      x
-    }
+  # Function to remove some of the axis labels in ggplot faceted plots
+  gtable_filter_remove <- function (x, name, trim = TRUE){
+    matches <- !(x$layout$name %in% name)
+    x$layout <- x$layout[matches, , drop = FALSE]
+    x$grobs <- x$grobs[matches]
+    if (trim) 
+      x <- gtable::gtable_trim(x)
+    x
+  }
+  
+  # Color scale for plotting
+  if ("colour" %in% names(args)) {
+    colour <- args[["colour"]]
+  } else {
+    colour <- c("#3B9AB2", "#E1AF00", "#BF0B98", "#4F94CD", "#9BCD9B")
+    # wesanderson::wes_palette("Zissou1", n = 4)
+  }
+  
+  if (length(obj) > length(colour)) colour <- c(colour, 1:(length(obj) - length(colour)))
+  
+  # Define years for the x-axis
+  if(!full){
+    start.year <- obj[[1]]$param$start.year
+    if(diff) start.year <- start.year - obj[[1]]$param$burn
+  } else {
+    start.year <- 2000
+  }
+  end.year <- obj[[1]]$param$start.year + obj[[1]]$param$yrs
+  
+  # Extract the trend data
+  proj.labels <- purrr::map_chr(.x = obj, .f = ~.x$param$label)
+  if(all(proj.labels == "")) proj.labels <- paste0("Projection ", stringr::str_pad(seq_along(obj), width = 2, pad = "0"))
+  
+  # Calculate difference between projections, if desired
+  if(diff){
     
-    # Color scale for plotting
-    if ("colour" %in% names(args)) {
-      colour <- args[["colour"]]
-    } else {
-      colour <- c("#BF0B98", "#E0B200", "#289DE0", "#0098A0", "#FF9B19FA", "#E6124B")
-    }
+    alpha_level <- 0.05
     
-    if (length(obj) > length(colour)) colour <- c(colour, 1:(length(obj) - length(colour)))
+    # Number of replicate proj
+    nreps <- obj[[1]]$param$n
     
-    # Define years for the x-axis
-    if(!full){
-      start.year <- obj[[1]]$param$start.year
-    } else {
-      start.year <- 2000
-    }
-    end.year <- obj[[1]]$param$start.year + obj[[1]]$param$yrs
+    # Number of years
+    n_yrs <- obj[[1]]$param$yrs + obj[[1]]$param$burn
     
-    # Extract the trend data
-    proj.labels <- purrr::map_chr(.x = obj, .f = ~.x$param$label)
-    if(all(proj.labels == "")) proj.labels <- paste0("Projection ", stringr::str_pad(seq_along(obj), width = 2, pad = "0"))
+    # All combinations of projections
+    allcombs <- combn(seq_along(obj), m = 2)
     
-    # Calculate difference between projections, if desired
-    if(diff){
+    # For all combinations
+    difftbl <- purrr::map(.x = 1:ncol(allcombs), .f = ~{
       
-      n <- obj[[1]]$param$n
-      allcombs <- combn(seq_along(obj), m = 2)
+      # Differences between projections
+      diffs <- obj[[allcombs[1,.x]]]$dat$tot - obj[[allcombs[2,.x]]]$dat$tot
       
-      # For all combinations
-      difftbl <- purrr::map(.x = 1:ncol(allcombs), .f = ~{
-        
-        # All combinations of individual projections
-        ndiff <- unique(expand.grid(1:n, 1:n))
-        
-        diffproj <- matrix(data = NA, nrow = nrow(ndiff), ncol = end.year-start.year + 1)
-        
-        pb <- progress::progress_bar$new(total = nrow(ndiff), width = 80, format = "Calculating differences [:bar] :percent eta: :eta", clear = TRUE)
-        
-        for(i in seq_len(nrow(ndiff))){
-          pb$tick()
-          diffproj[i, ] <- obj[[allcombs[1,.x]]]$proj$tbl[year >= start.year & cohort == "North Atlantic right whales" & prj == ndiff[i,1], N] -
-            obj[[allcombs[2,.x]]]$proj$tbl[year >= start.year &  cohort == "North Atlantic right whales" & prj == ndiff[i,2], N]
-        }
-        
-        data.table::data.table(year = seq(start.year, end.year),
-                               mean = colMeans(diffproj),
-                               lwr = apply(X = diffproj, MARGIN = 2, FUN = function(x) quantile(x, 0.025)),
-                               uppr = apply(X = diffproj, MARGIN = 2, FUN = function(x) quantile(x, 0.975)),
-                               label = paste0("[",proj.labels[allcombs[1,.x]], "] vs. [", proj.labels[allcombs[2,.x]], "]"))
-        
-      }) |> data.table::rbindlist()
+      if(all.diff){
+      diffs.dt <- data.table::data.table(diffs)
+      diffs.dt$proj <- row.names(diffs)
+      diffs.dt <- diffs.dt |> tidyr::pivot_longer(!proj) |> dplyr::rename(year = name) |> dplyr::mutate(year = as.numeric(stringr::str_replace(string = year, pattern = "yr ", replacement = ""))+2019)
+      }
       
+      # Difference between means - equivalent to 2-sample t-test on the means
+      mean_diff <- apply(obj[[allcombs[1,.x]]]$dat$tot, 2, mean) - apply(obj[[allcombs[2,.x]]]$dat$tot, 2, mean)
+      
+      if(!cohen){
+      se_diff <- sqrt ((apply(obj[[allcombs[1,.x]]]$dat$tot, 2, var) / nreps) + (apply(obj[[allcombs[2,.x]]]$dat$tot, 2, var) / nreps))
+      # t statistic
+      tstat <- stats::qt(1 - alpha_level / 2, (nreps * 2) - 2) * se_diff
+      lcl_diff <- mean_diff - tstat
+      ucl_diff <- mean_diff + tstat
+      
+      out <- data.table::data.table(year = seq(start.year, end.year),
+                                    mean = mean_diff,
+                                    lwr = lcl_diff,
+                                    uppr = ucl_diff,
+                                    label = paste0("[",proj.labels[allcombs[1,.x]], "] – [", proj.labels[allcombs[2,.x]], "]"))
+      
+      if(all.diff)  out <- dplyr::left_join(diffs.dt, out, by = "year") 
+      
+      } else {
+        
+      var_pooled <- (apply(obj[[allcombs[1,.x]]]$dat$tot, 2, var) + apply(obj[[allcombs[2,.x]]]$dat$tot, 2, var)) / 2
+      CohensD <- mean_diff / sqrt(var_pooled) 
+      
+      out <- data.table::data.table(year = seq(start.year, end.year),
+                                    mean = mean_diff,
+                                    cohen = CohensD,
+                                    label = paste0("[",proj.labels[allcombs[1,.x]], "] – [", proj.labels[allcombs[2,.x]], "]"))
+      
+      }
+      out
+
+    }) |> data.table::rbindlist()
+    
+    if(cohen){
       p <- ggplot2::ggplot() +
-        {if (interval) ggplot2::geom_ribbon(data = difftbl, 
-                                            aes(x = year, y = mean, ymin = lwr, ymax = uppr), alpha = 0.15)} +
-        ggplot2::geom_line(data = difftbl, aes(x = year, y = mean), linetype = "solid", linewidth = 0.8) +
-        ggplot2::scale_y_continuous(limits = c(min(0, min(difftbl$lwr)), max(0, max(difftbl$uppr)))) +
+        ggplot2::geom_ribbon(data = data.frame(year = seq(start.year, end.year),
+                                               lwr = -0.8, uppr = 0.8),
+                             aes(x = year, ymin = lwr, ymax = uppr), fill = "firebrick",
+                             alpha = 0.2) +
+        ggplot2::geom_ribbon(data = data.frame(year = seq(start.year, end.year),
+                                               lwr = -0.5, uppr = 0.5),
+                             aes(x = year, ymin = lwr, ymax = uppr), fill = "firebrick",
+                             alpha = 0.2) +
+        ggplot2::geom_ribbon(data = data.frame(year = seq(start.year, end.year),
+                                               lwr = -0.2, uppr = 0.2),
+                                               aes(x = year, ymin = lwr, ymax = uppr), fill = "firebrick",
+                             alpha = 0.2) +
+        ggplot2::geom_line(data = difftbl, aes(x = year, y = cohen, colour = label), linetype = "solid", linewidth = 0.8) +
+        ggplot2::scale_y_continuous(limits = c(-1,1)) +
         ggplot2::geom_abline(intercept = 0, slope = 0, linetype = "dotted") +
-        xlab("") + ylab ("Difference in predicted NARW abundance") +
-        ggplot2::facet_wrap(~label, scales = scales, ncol = ncol) +
+        xlab("") + ylab ("Cohen's D") +
         ggplot2::scale_x_continuous(breaks = pretty(c(start.year, end.year), n = 10)) +
-        ggplot2::scale_y_continuous(breaks = pretty(c(min(0, min(difftbl$lwr)), max(0, max(difftbl$uppr))), n = 10)) +
-        theme_narw()
-
-      suppressWarnings(print(p))
-
+        ggplot2::scale_colour_manual(values = pals::parula(ncol(allcombs)), name = "") +
+        theme_narw() +
+        ggplot2::theme(legend.position = "bottom", 
+                       legend.key = ggplot2::element_blank())
       
     } else {
-
+      
+    p <- ggplot2::ggplot() +
+      {if(all.diff) ggplot2::geom_path(data = difftbl, aes(x = year, y = value, group = proj), alpha = 0.05)} +
+      {if (interval) ggplot2::geom_ribbon(data = difftbl, 
+                                          aes(x = year, y = mean, ymin = lwr, ymax = uppr), alpha = 0.25, fill = "firebrick")} +
+      ggplot2::geom_line(data = difftbl, aes(x = year, y = mean), linetype = "solid", linewidth = 0.8) +
+      ggplot2::scale_y_continuous(limits = c(min(0, min(difftbl$lwr)), max(0, max(difftbl$uppr)))) +
+      ggplot2::geom_abline(intercept = 0, slope = 0, linetype = "dotted") +
+      xlab("") + ylab ("Difference in predicted NARW abundance") +
+      ggplot2::facet_wrap(~label, scales = scales, ncol = ncol) +
+      ggplot2::scale_x_continuous(breaks = pretty(c(start.year, end.year), n = 10)) +
+      {if(all.diff) ggplot2::scale_y_continuous(breaks = pretty(c(min(0, min(difftbl$value)), max(0, max(difftbl$value))), n = 10)) } +
+      {if(!all.diff) ggplot2::scale_y_continuous(breaks = pretty(c(min(0, min(difftbl$lwr)), max(0, max(difftbl$uppr))), n = 10)) } +
+      theme_narw()
+    }
+    
+    suppressWarnings(print(p))
+    
+    
+  } else {
+    
     df <- purrr::map2(
       .x = obj, 
       .y = proj.labels,
@@ -194,7 +259,7 @@ plot.narwproj <- function(...,
       {if(noaa) ggnewscale::new_scale_fill() } +
       {if(noaa) ggnewscale::new_scale_colour()} + 
       {if (interval & shade) ggplot2::geom_ribbon(data = df, aes(x = year, y = mean, ymin = lwr, ymax = uppr, fill = label, group = label),
-                                                  alpha = 0.3)} +
+                                                  alpha = alpha)} +
       {if (interval & !shade) ggplot2::geom_line(data = df, aes(x = year, y = lwr, colour = label, group = label), size = 0.5, linetype = "dotdash")} +
       {if (interval & !shade) ggplot2::geom_line(data = df, aes(x = year, y = uppr, colour = label, group = label), size = 0.5, linetype = "dotdash")} +
       ggplot2::geom_path(data = df, aes(x = year, y = mean, colour = label, group = label), linewidth = 0.8, na.rm = TRUE) +
